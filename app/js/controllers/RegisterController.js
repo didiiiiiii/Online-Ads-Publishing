a206a166ec640ac40b4774676afae0402beed024
didiiiiiii <delyana_86@yahoo.com>
2017-12-22 17:28:51 +0200

    lab 6; register user

--- app/js/controllers/RegisterController.js
+++ app/js/controllers/RegisterController.js
@@ -2,7 +2,21 @@
 
 //The RightSidebarController controls the content displayed in the right RightSidebarController
 app.controller('RegisterController',
-    function($scope, categoriesService, townsService) {
-        //TODO
+    function($scope, $location, categoriesService, townsService, authService, notifyService) {
+        $scope.userData = {townId: null};
+
+        //TODO: load the towns in $scope (for the towns drop-down list)
+
+        $scope.register = function(userData) {
+            authService.register(userData,
+                function success() {
+                    //TODO: display success message
+                    //TODO: redirect to login screen
+                },
+                function error(err) {
+                    notifyService.showError("User registration failed", err);
+                }
+            );
+        };
     }
 );
